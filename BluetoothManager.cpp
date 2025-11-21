474c194db4e8fb86b6a045c4240d88cb51a8e068
DESKTOP-332SS1S\GSM <bleaf0513@gmail.com>
2025-11-20 20:47:42 -0800

    no message

--- BluetoothManager.cpp
+++ BluetoothManager.cpp
@@ -1,85 +1,189 @@
-#include <QBluetoothDeviceDiscoveryAgent>
-#include <QBluetoothSocket>
-#include <QBluetoothUuid>
-#include <QBluetoothDeviceInfo>
-#include <QMap>
-#include <QDebug>
-#include <QObject>
 #include "BluetoothManager.h"
+#include <QLowEnergyDescriptor>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
+const QBluetoothUuid FTMS_SERVICE_UUID("00001826-0000-1000-8000-00805f9b34fb");
+const QBluetoothUuid ROWER_DATA_CHAR_UUID("00002AD3-0000-1000-8000-00805f9b34fb");
+const QBluetoothUuid RESISTANCE_LEVEL_CHAR_UUID("00002AD6-0000-1000-8000-00805f9b34fb");
+static void logWarningToFile(const QString &message)
+{
+    QFile file("bluetooth_warnings.log"); // log file in working directory
+    if (file.open(QIODevice::Append | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << message<< "\n";
+        file.close();
+    }
+
+}
 BluetoothManager::BluetoothManager(QObject *parent) : QObject(parent)
 {
     discoveryAgent = new QBluetoothDeviceDiscoveryAgent(this);
 
-    // Connect signals
-    connect(discoveryAgent, &QBluetoothDeviceDiscoveryAgent::deviceDiscovered, this, &BluetoothManager::deviceDiscovered);
-    connect(discoveryAgent, &QBluetoothDeviceDiscoveryAgent::finished, this, &BluetoothManager::scanFinished);
+    connect(discoveryAgent, &QBluetoothDeviceDiscoveryAgent::deviceDiscovered,
+            this, &BluetoothManager::deviceDiscovered);
+    connect(discoveryAgent, &QBluetoothDeviceDiscoveryAgent::finished,
+            this, &BluetoothManager::scanFinished);
+
 }
 
-Q_INVOKABLE void BluetoothManager::startScan() {
-    qWarning() << "Starting Bluetooth device scan from QML...";
-    discoveryAgent->start();
+void BluetoothManager::startScan()
+{
+    consoles.clear();
+    qDebug() << "Starting Bluetooth scan for KP3902 consoles...";
+    discoveryAgent->start(QBluetoothDeviceDiscoveryAgent::LowEnergyMethod);
 }
 
-void BluetoothManager::deviceDiscovered(const QBluetoothDeviceInfo &device) {
-    qDebug() << "Discovered device:" << device.name() << device.address().toString();
-    // Check if the device name matches the KP3902 or if it's in a list of known console addresses
-    if (device.name() == "KP3902") {
-        connectToDevice(device.address());
+void BluetoothManager::deviceDiscovered(const QBluetoothDeviceInfo &device)
+{
+    qWarning()<<device.name();
+
+    if (device.name() == "KP3902" ) {
+        if (consoles.size() >= 10)
+            return;
+
+        KayakConsole console;
+        console.name = device.name();
+        console.address = device.address().toString();
+        consoles.append(console);
+        qDebug() << "Added KP3902 console:" << console.address;
     }
 }
 
-void BluetoothManager::scanFinished() {
-    qDebug() << "Device scan finished.";
+void BluetoothManager::scanFinished()
+{
+    qDebug() << "Scan finished. Connecting to consoles...";
+    for (int i = 0; i < consoles.size(); ++i)
+        connectConsole(&consoles[i]);
 }
 
+void BluetoothManager::connectConsole(KayakConsole *console)
+{
+    logWarningToFile("temp1");
+    QBluetoothAddress addr(console->address);
+    console->controller = QLowEnergyController::createCentral(QBluetoothDeviceInfo(addr,console->name,1), this);
 
-void BluetoothManager::connectToDevice(const QBluetoothAddress &address) {
-    if (activeSockets.contains(address)) {
-        qDebug() << "Already connected to this device.";
-        return;  // Skip if already connected
+    connect(console->controller, &QLowEnergyController::connected, [console]() {
+        qDebug() << console->name << "connected. Discovering services...";
+        console->controller->discoverServices();
+    });
+
+    connect(console->controller, &QLowEnergyController::disconnected, [console]() {
+        qDebug() << console->name << "disconnected.";
+    });
+    logWarningToFile("temp2");
+    connect(console->controller, &QLowEnergyController::serviceDiscovered,
+            [this, console](const QBluetoothUuid &uuid) {
+                if (uuid == FTMS_SERVICE_UUID) {
+                    console->ftmsService = console->controller->createServiceObject(uuid, this);
+                    if (!console->ftmsService)
+                        return;
+                    logWarningToFile(uuid.toString());
+                    connect(console->ftmsService, &QLowEnergyService::stateChanged,
+                            [this, console](QLowEnergyService::ServiceState s) {
+                        logWarningToFile("temp4.1");
+                              //  if (s == QLowEnergyService::ServiceDiscovered) {
+                                    // Assign characteristics
+                                    console->metricsChar = console->ftmsService->characteristic(ROWER_DATA_CHAR_UUID);
+                                    console->resistanceLevelChar = console->ftmsService->characteristic(RESISTANCE_LEVEL_CHAR_UUID);
+
+                                    if (!console->resistanceLevelChar.isValid()) {
+                                        qDebug() << "Resistance characteristic not found! Listing all characteristics:";
+                                        for (const QLowEnergyCharacteristic &c : console->ftmsService->characteristics())
+                                            qDebug() << c.uuid().toString();
+                                    }
+                                    logWarningToFile("temp4.2");
+                                    // Enable notifications for metrics
+                                    if (console->metricsChar.isValid()) {
+                                        QLowEnergyDescriptor desc = console->metricsChar.descriptor(
+                                            QBluetoothUuid(QLatin1String("00001826-0000-1000-8000-00805f9b34fb")));
+                                        if (desc.isValid())
+                                        {    console->ftmsService->writeDescriptor(desc, QByteArray::fromHex("0100"));
+                                            logWarningToFile("temp4.3");
+                                        }
+                                    }
+
+                                    connect(console->ftmsService, &QLowEnergyService::characteristicChanged,
+                                            this, &BluetoothManager::characteristicChanged);
+
+                                    qDebug() << console->name << "service setup complete.";
+
+                             //   }
+                              //  else
+                             //   {
+                              //      logWarningToFile("temp4.3");
+                             //   }
+                            });
+
+                    console->ftmsService->discoverDetails();
+                }
+            });
+
+    console->controller->connectToDevice();
+}
+
+void BluetoothManager::characteristicChanged(const QLowEnergyCharacteristic &c, const QByteArray &value)
+{
+    logWarningToFile("temp5");
+    for (auto &console : consoles) {
+        // if (c.uuid() == console.metricsChar.uuid() && value.size() >= 18) {
+        const quint8 *data = reinterpret_cast<const quint8*>(value.constData());
+
+        console.strokeRate    = data[3] | (data[4] << 8);
+        console.strokeCount   = data[13] | (data[14] << 8);
+        console.avgStrokeRate = data[15] | (data[16] << 8);
+        console.totalDistance = (data[5] | (data[6] << 8)) / 10.0;
+        console.instantPace   = (data[1] | (data[2] << 8)) / 100.0;
+        console.elapsedTime   = data[9] | (data[10] << 8);
+        console.remainingTime = data[11] | (data[12] << 8);
+        console.energyPerHour = data[17] | (data[18] << 8);
+        console.power         = data[7] | (data[8] << 8);
+
+        QString temp="StrokeRate:"+QString::number(console.strokeRate)+"\n";
+        temp+="StrokeCount:"+QString::number(console.strokeCount)+"\n";
+        temp+="AvgStrokeRate:"+QString::number(console.avgStrokeRate)+"\n";
+        temp+="Distance:"+QString::number(console.totalDistance)+"\n";
+        temp+="Elapsed:"+QString::number(console.elapsedTime)+"\n";
+        temp+="Power:"+QString::number(console.power)+"\n";
+        logWarningToFile(temp);
+        //        }
+    }
+}
+
+void BluetoothManager::setResistance(int consoleIndex, int level)
+{
+    if (consoleIndex < 0 || consoleIndex >= consoles.size())
+        return;
+
+    KayakConsole *console = &consoles[consoleIndex];
+    if (!console->resistanceLevelChar.isValid()) {
+        qWarning() << "Cannot write resistance: invalid characteristic for" << console->name;
+        return;
     }
 
-    QBluetoothSocket *socket = new QBluetoothSocket(this);
-    QBluetoothUuid sppUuid("00001101-0000-1000-8000-00805F9B34FB");
-    socket->connectToService(address, sppUuid);
-
-    connect(socket, &QBluetoothSocket::connected, this, &BluetoothManager::onConnected);
-    connect(socket, &QBluetoothSocket::readyRead, this, &BluetoothManager::onDataReceived);
-    connect(socket, &QBluetoothSocket::disconnected, this, &BluetoothManager::onDisconnected);
-
-    activeSockets[address] = socket; // Add to active connections map
-    qDebug() << "Attempting to connect to device" << address.toString();
+    QByteArray data;
+    data.append(static_cast<char>(level & 0xFF));
+    console->ftmsService->writeCharacteristic(console->resistanceLevelChar,
+                                              data,
+                                              QLowEnergyService::WriteWithoutResponse);
 }
 
-void BluetoothManager::onConnected() {
-    QBluetoothSocket *socket = qobject_cast<QBluetoothSocket *>(sender());
-    QBluetoothAddress address = socket->peerAddress();
-    qDebug() << "Connected to device" << address.toString();
+QVariantMap BluetoothManager::getMetrics(int consoleIndex)
+{
+    QVariantMap map;
+    if (consoleIndex < 0 || consoleIndex >= consoles.size())
+        return map;
+
+    auto &c = consoles[consoleIndex];
+    map["strokeRate"] = c.strokeRate;
+    map["strokeCount"] = c.strokeCount;
+    map["avgStrokeRate"] = c.avgStrokeRate;
+    map["distance"] = c.totalDistance;
+    map["instantPace"] = c.instantPace;
+    map["elapsedTime"] = c.elapsedTime;
+    map["remainingTime"] = c.remainingTime;
+    map["energyPerHour"] = c.energyPerHour;
+    map["power"] = c.power;
+
+    return map;
 }
-
-void BluetoothManager::onDataReceived() {
-    QBluetoothSocket *socket = qobject_cast<QBluetoothSocket *>(sender());
-    QByteArray data = socket->readAll();
-    QBluetoothAddress address = socket->peerAddress();
-    qDebug() << "Received data from" << address.toString() << ":" << data;
-
-    // Parse and handle data here based on the KP3902 protocol
-    parseKP3902Data(data);
-}
-
-void BluetoothManager::onDisconnected() {
-    QBluetoothSocket *socket = qobject_cast<QBluetoothSocket *>(sender());
-    QBluetoothAddress address = socket->peerAddress();
-    qDebug() << "Disconnected from" << address.toString();
-
-    activeSockets.remove(address); // Remove the disconnected socket from active connections
-    socket->deleteLater(); // Clean up socket object
-}
-
-void BluetoothManager::parseKP3902Data(const QByteArray &data) {
-    // Implement the protocol-specific data parsing here
-    // For example, if data is in JSON format:
-    // QJsonDocument doc = QJsonDocument::fromJson(data);
-    // Handle data accordingly
-}
-
--- BluetoothManager.h
+++ BluetoothManager.h
@@ -1,31 +1,54 @@
-#include <QBluetoothDeviceDiscoveryAgent>
-#include <QBluetoothSocket>
-#include <QBluetoothUuid>
-#include <QBluetoothDeviceInfo>
-#include <QMap>
-#include <QDebug>
+#ifndef BLUETOOTHMANAGER_H
+#define BLUETOOTHMANAGER_H
+
 #include <QObject>
-class BluetoothManager : public QObject {
+#include <QLowEnergyController>
+#include <QLowEnergyService>
+#include <QLowEnergyCharacteristic>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include <QBluetoothDeviceInfo>
+#include <QVariantMap>
+
+struct KayakConsole {
+    QString name;
+    QString address;
+    QLowEnergyController *controller = nullptr;
+    QLowEnergyService *ftmsService = nullptr;
+    QLowEnergyCharacteristic metricsChar;
+    QLowEnergyCharacteristic resistanceLevelChar;
+
+    // Rower Metrics
+    int strokeRate = 0;
+    int strokeCount = 0;
+    int avgStrokeRate = 0;
+    double totalDistance = 0.0;
+    double instantPace = 0.0;
+    int elapsedTime = 0;
+    int remainingTime = 0;
+    int energyPerHour = 0;
+    int power = 0;
+};
+
+class BluetoothManager : public QObject
+{
     Q_OBJECT
 public:
-
     explicit BluetoothManager(QObject *parent = nullptr);
+
     Q_INVOKABLE void startScan();
+    Q_INVOKABLE void setResistance(int consoleIndex, int level);
+    Q_INVOKABLE QVariantMap getMetrics(int consoleIndex);
+
 private slots:
     void deviceDiscovered(const QBluetoothDeviceInfo &device);
-
     void scanFinished();
+    void characteristicChanged(const QLowEnergyCharacteristic &c, const QByteArray &value);
 
 private:
     QBluetoothDeviceDiscoveryAgent *discoveryAgent;
-    QMap<QBluetoothAddress, QBluetoothSocket*> activeSockets; // Map to track multiple connections
+    QList<KayakConsole> consoles;
 
-    void connectToDevice(const QBluetoothAddress &address) ;
-
-    void onConnected() ;
-
-    void onDataReceived();
-
-    void onDisconnected();
-    void parseKP3902Data(const QByteArray &data);
+    void connectConsole(KayakConsole *console);
 };
+
+#endif // BLUETOOTHMANAGER_H
